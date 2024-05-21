80b392c11f58d70dda2da4c956d9856845fa01d1
Alex <alex.rodriguescosta@student.supsi.ch>
2024-05-21 13:44:21 +0200

    - Fixed graphical issue when opening new grid

--- frontend/src/main/java/ch/supsi/connectfour/frontend/view/BoardView.java
+++ frontend/src/main/java/ch/supsi/connectfour/frontend/view/BoardView.java
@@ -2,34 +2,18 @@ package ch.supsi.connectfour.frontend.view;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
-import java.net.URL;
 
-public class BoardView implements Viewable{
+public class BoardView implements Viewable {
     @FXML
     private GridPane gridPane;
 
     @FXML
-    private ImageView boardLayer;
-
-    /*
-    @FXML
     public void initialize(){
         gridPane.setStyle("-fx-grid-lines-visible: true;");
-        this.clear();
-
-        URL imageUrl = getClass().getResource("/images/board.png");
-        if(imageUrl == null){
-            System.err.println("Error while loading board image");
-            return;
-        }
-        Image image = new Image(imageUrl.toExternalForm());
-        boardLayer.setImage(image);
+        this.clear(); //valutare di cambiare direttamente il file fxml
     }
-    */
     public void setCellText(int row, int column, String text) {
         for (javafx.scene.Node node : gridPane.getChildren()) {
             if (node instanceof AnchorPane anchorPane && GridPane.getRowIndex(node) == row && GridPane.getColumnIndex(node) == column) {
@@ -42,9 +26,8 @@ public class BoardView implements Viewable{
             }
         }
     }
-
     @Override
-    public void clear() {
+    public void clear(){
         for (javafx.scene.Node node : gridPane.getChildren()) {
             if (node instanceof AnchorPane anchorPane) {
                 for (javafx.scene.Node child : anchorPane.getChildren()) {

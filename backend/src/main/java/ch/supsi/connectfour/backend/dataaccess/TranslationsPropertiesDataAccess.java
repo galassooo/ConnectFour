f8aaa6ec1bb47ef9c857273779edc21eb4688f88
Alex <alex.rodriguescosta@student.supsi.ch>
2024-05-28 12:59:27 +0200

    - Fixed loading labels

--- backend/src/main/java/ch/supsi/connectfour/backend/dataaccess/TranslationsPropertiesDataAccess.java
+++ backend/src/main/java/ch/supsi/connectfour/backend/dataaccess/TranslationsPropertiesDataAccess.java
@@ -6,7 +6,12 @@ import org.jetbrains.annotations.NotNull;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
@@ -14,9 +19,8 @@ import java.util.jar.JarFile;
 import static java.util.ResourceBundle.Control.FORMAT_DEFAULT;
 
 public class TranslationsPropertiesDataAccess implements TranslationsDataAccessInterface {
-    // TODO: figure out why this explodes with File.separator
-    //TODO: implementare separator lol
-    private static final String LABELS_PATH = "/i18n/labels";
+    // TODO: figure out if file separator is needed
+    private static final String LABELS_PATH = "i18n/labels";
     private static final String SUPPORTED_LANGUAGES_PATH = "/supported-languages.properties";
 
     private static TranslationsPropertiesDataAccess myself;
@@ -58,17 +62,15 @@ public class TranslationsPropertiesDataAccess implements TranslationsDataAccessI
         final Properties translations = new Properties();
         try {
             Enumeration<URL> resources = getClass().getClassLoader().getResources(LABELS_PATH);
-            // TODO: remove while, there's only one loop ever
-            while (resources.hasMoreElements()) {
-                URL resource = resources.nextElement();
-                if (resource.getProtocol().equals("jar")) {
-                    // connectfour-jar-with-dependencies.jar!/i18n/labels
-                    // When we are working from a jar this method will always be called
-                    processJarResource(resource, locale, translations);
-                } else {
-                    // If we are running the application from an IDE or not from a Jar
-                    processResource(resource, locale, translations);
-                }
+
+            URL resource = resources.nextElement();
+            if (resource.getProtocol().equals("jar")) {
+                // connectfour-jar-with-dependencies.jar!/i18n/labels
+                // When we are working from a jar this method will always be called
+                processJarResource(resource, locale, translations);
+            } else {
+                // If we are running the application from an IDE or not from a Jar
+                processResource(resource, locale, translations);
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -82,12 +84,16 @@ public class TranslationsPropertiesDataAccess implements TranslationsDataAccessI
         for (File file : Objects.requireNonNull(directory.listFiles())) {
             String[] parts = file.getName().split("_");
             String baseName = String.format("%s.%s_%s", LABELS_PATH, parts[0], parts[1]);
-            ResourceBundle bundle = ResourceBundle.getBundle(baseName, locale, ResourceBundle.Control.getNoFallbackControl(FORMAT_DEFAULT));
-            loadTranslationsFromBundle(bundle, translations);
+            try {
+                ResourceBundle bundle = bundle = ResourceBundle.getBundle(baseName, locale, ResourceBundle.Control.getNoFallbackControl(FORMAT_DEFAULT));
+                loadTranslationsFromBundle(bundle, translations);
+            } catch (MissingResourceException e) {
+                handleMissingResourceException(resource, locale, translations);
+            }
         }
     }
 
-    private void processJarResource(URL resource, Locale locale, Properties translations) throws IOException {
+    private void processJarResource(URL resource, Locale locale, Properties translations) {
         // The path of the jar is something like jar:file:/../../../connectfour-jar-with-dependencies.jar![path of the resource]
         String jarPath = resource.getPath().substring(5, resource.getPath().indexOf("!")); // Getting the path of the jar
         try (JarFile jarFile = new JarFile(jarPath)) {
@@ -99,15 +105,22 @@ public class TranslationsPropertiesDataAccess implements TranslationsDataAccessI
                 String entryName = entry.getName();
                 if (entryName.startsWith(LABELS_PATH) && entryName.endsWith(".properties")) {
                     // ex: i18n/labels/preferences_labels_it_CH.properties
+                    // TODO: check if problematic on other OSs
                     String[] parts = entryName.split("/");
                     // Handles the last part of the name e.g. preferences_labels_it_CH.properties
                     String[] lastParts = parts[2].split("_");
                     String baseName = parts[0] + "." + parts[1] + "." + (lastParts[0] + "_" + lastParts[1]);
                     System.out.println(baseName);
-                    ResourceBundle bundle = ResourceBundle.getBundle(baseName, locale, ResourceBundle.Control.getNoFallbackControl(FORMAT_DEFAULT));
-                    loadTranslationsFromBundle(bundle, translations);
+                    try {
+                        ResourceBundle bundle = ResourceBundle.getBundle(baseName, locale, ResourceBundle.Control.getNoFallbackControl(FORMAT_DEFAULT));
+                        loadTranslationsFromBundle(bundle, translations);
+                    } catch (MissingResourceException e) {
+                        handleMissingResourceException(resource, locale, translations);
+                    }
                 }
             }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
@@ -117,24 +130,21 @@ public class TranslationsPropertiesDataAccess implements TranslationsDataAccessI
         }
     }
 
-    private void handleMissingResourceException(@NotNull Locale locale, Properties translations) {
+    private void handleMissingResourceException(URL resource, @NotNull Locale locale, Properties translations) {
         System.err.println("Unsupported language tag: " + locale.toLanguageTag());
         List<String> supportedLanguageTags = this.getSupportedLanguageTags();
         if (!supportedLanguageTags.isEmpty()) {
             String fallbackLanguageTag = supportedLanguageTags.get(0);
             System.err.println("Falling back to: " + fallbackLanguageTag);
             Locale fallbackLocale = Locale.forLanguageTag(fallbackLanguageTag);
-            try {
-                URL resource = this.getClass().getResource(LABELS_PATH);
-                if (resource != null) {
-                    Path path = Paths.get(resource.toURI());
-                    Files.walk(path)
-                            .filter(Files::isRegularFile)
-                            .filter(p -> p.toString().endsWith(".properties"))
-                            .forEach(p -> loadTranslationsFromFile(p, fallbackLocale, translations));
-                }
-            } catch (IOException | URISyntaxException e) {
-                e.printStackTrace();
+            if (resource.getProtocol().equals("jar")) {
+                // connectfour-jar-with-dependencies.jar!/i18n/labels
+                // When we are working from a jar this method will always be called
+                processJarResource(resource, fallbackLocale, translations);
+            } else {
+                // If we are running the application from an IDE or not from a Jar
+                processResource(resource, fallbackLocale, translations);
             }
         }
     }
+}

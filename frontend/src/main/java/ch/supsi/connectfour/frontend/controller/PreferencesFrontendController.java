243c91d70a0c46a45cdd54183496e0c078321a2a
galassoo <160861453+galassoo@users.noreply.github.com>
2024-05-28 18:27:17 +0200

    fixed preferences saving/load

--- frontend/src/main/java/ch/supsi/connectfour/frontend/controller/PreferencesFrontendController.java
+++ frontend/src/main/java/ch/supsi/connectfour/frontend/controller/PreferencesFrontendController.java
@@ -8,8 +8,13 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
 import java.util.stream.Stream;
 
@@ -24,6 +29,8 @@ public class PreferencesFrontendController {
     private final Stage stage = new Stage();
 
     private final static String BUNDLE_NAME = "i18n/UI.ui_labels";
+
+    private final static String shapePreferencePattern = "/images/symbols/%s.PNG";
 
     public static PreferencesFrontendController getInstance() {
         if (instance == null) {
@@ -59,10 +66,10 @@ public class PreferencesFrontendController {
                 backendController.setPreference(new AbstractMap.SimpleEntry<>("player-two-color", playerTwoColor));
 
                 String playerOneShape = preferencesView.getPlayerOneShape();
-                backendController.setPreference(new AbstractMap.SimpleEntry<>("player-one-shape", playerOneShape));
+                backendController.setPreference(new AbstractMap.SimpleEntry<>("player-one-shape",String.format(shapePreferencePattern,playerOneShape)));
 
                 String playerTwoShape = preferencesView.getPlayerTwoShape();
-                backendController.setPreference(new AbstractMap.SimpleEntry<>("player-two-shape", playerTwoShape));
+                backendController.setPreference(new AbstractMap.SimpleEntry<>("player-two-shape", String.format(shapePreferencePattern,playerTwoShape)));
 
                 stage.close();
             });
@@ -82,9 +89,20 @@ public class PreferencesFrontendController {
         this.preferencesView.setLanguages(this.translationsController.getSupportedLanguages());
 
         //Guardare nella directory images/symbols/.... e fare uno split e prendere solo la parte prima dell PNG
-        //<String> validSymbols = Stream.of(Symbol2.values()).map(Enum::toString).toList();
-
-        //this.preferencesView.setShapes(validSymbols);
+        List<String> validSymbols = new ArrayList<>();
+        URL resource = getClass().getResource("/images/symbols");
+        if (resource != null) {
+            try (Stream<Path> paths = Files.walk(Path.of(resource.toURI()))) {
+                paths.filter(Files::isRegularFile)
+                        .map(path -> path.getFileName().toString())
+                        .filter(fileName -> fileName.toLowerCase().endsWith(".png"))
+                        .map(fileName -> fileName.substring(0, fileName.length() - 4)) // remove the .png suffix
+                        .forEach(validSymbols::add);
+            } catch (IOException | URISyntaxException e) {
+                e.printStackTrace();
+            }
+        }
+        this.preferencesView.setShapes(validSymbols);
     }
 
     public void managePreferences() {

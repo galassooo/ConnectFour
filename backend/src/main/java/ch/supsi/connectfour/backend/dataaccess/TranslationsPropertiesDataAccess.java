04c9aa308374c9dca5429a297d66ab1ecf8beba9
Alex <alex.rodriguescosta@student.supsi.ch>
2024-05-29 09:18:31 +0200

    - Added new dependency to make handling jars more intuitive - Attempted to make loading labels independent from the OS where they are loaded from and from the app. being ran in the IDE or from JAR

--- backend/src/main/java/ch/supsi/connectfour/backend/dataaccess/TranslationsPropertiesDataAccess.java
+++ backend/src/main/java/ch/supsi/connectfour/backend/dataaccess/TranslationsPropertiesDataAccess.java
@@ -2,9 +2,13 @@ package ch.supsi.connectfour.backend.dataaccess;
 
 import ch.supsi.connectfour.backend.business.translations.TranslationsDataAccessInterface;
 import org.jetbrains.annotations.NotNull;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.net.URLDecoder;
@@ -19,7 +23,7 @@ import java.util.jar.JarFile;
 import static java.util.ResourceBundle.Control.FORMAT_DEFAULT;
 
 public class TranslationsPropertiesDataAccess implements TranslationsDataAccessInterface {
-    private static final String LABELS_PATH = "i18n/labels";
+    private static final String LABELS_PATH = String.format("i18n%slabels%s", File.separator, File.separator);
 
     private static final String supportedLanguagesPath = "/supported-languages.properties";
     private static final String UI_LABELS_PATH = "i18n/UI/ui_labels";
@@ -63,111 +67,47 @@ public class TranslationsPropertiesDataAccess implements TranslationsDataAccessI
     @Override
     public Properties getTranslations(Locale locale) {
         Properties translations = new Properties();
+
+        List<ResourceBundle> bundles = getResourceBundlesForLocale(locale);
+        // It means it failed to load translations for the given locale, fallback to a default one
+        if (bundles.isEmpty()) {
+            Locale fallbackLocale = Locale.forLanguageTag(this.getSupportedLanguageTags().get(0));
+            System.err.printf("Invalid locale: %s. Loading new locale: %s\n", locale.toString(), fallbackLocale.toString());
+            bundles = getResourceBundlesForLocale(fallbackLocale);
+        }
+        bundles.forEach((b) -> {
+            for (String key : b.keySet()) {
+                translations.setProperty(key, b.getString(key));
+            }
+        });
+
+        return translations;
+    }
+    private List<ResourceBundle> getResourceBundlesForLocale(Locale locale) {
+        PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+        List<ResourceBundle> resourceBundles = new ArrayList<>();
         try {
-            Enumeration<URL> resources = getClass().getClassLoader().getResources(LABELS_PATH);
-            while (resources.hasMoreElements()) {
-                URL resource = resources.nextElement();
-                if (resource.getProtocol().equals("jar")) {
-                    processJarResource(resource, locale, translations);
-                } else {
-                    processResource(resource, locale, translations);
+            Resource[] resources = resolver.getResources(String.format("classpath:%s*.properties", "i18n/labels/"));
+            for (Resource resource : resources) {
+                String filename = resource.getFilename();
+                if (filename != null && filename.contains("_" + locale.toString() + ".properties")) {
+                    try (InputStreamReader reader = new InputStreamReader(resource.getInputStream(), StandardCharsets.UTF_8)) {
+                        ResourceBundle resourceBundle = new PropertyResourceBundle(reader);
+                        resourceBundles.add(resourceBundle);
+                    }
                 }
             }
+
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return translations;
+        return resourceBundles;
     }
 
-    private void processResource(URL resource, Locale locale, Properties translations) {
-        try {
-            Path path = Paths.get(resource.toURI());
-            Files.walk(path)
-                    .filter(Files::isRegularFile)
-                    .filter(p -> p.toString().endsWith(".properties"))
-                    .forEach(p -> loadTranslationsFromFile(p, locale, translations));
-        } catch (IOException | URISyntaxException e) {
-            e.printStackTrace();
-        }
-    }
-
+    // TODO: fix this. It does not work if the locale is not valid. Probably better to make the method above more generic so that same one can be used for these
     @Override
     public ResourceBundle getUIResourceBundle(Locale locale) {
         String baseName = UI_LABELS_PATH;
         return ResourceBundle.getBundle(baseName, locale, ResourceBundle.Control.getNoFallbackControl(FORMAT_DEFAULT));
-    }
-
-    private void loadTranslationsFromFile(@NotNull Path path, Locale locale, Properties translations) {
-        String fileName = path.getFileName().toString();
-        String[] parts = fileName.split("_");
-        if (parts.length >= 2) {
-            String baseName = String.format(LABELS_PATH_2, parts[0], parts[1]);
-            try {
-                ResourceBundle bundle = ResourceBundle.getBundle(baseName, locale, ResourceBundle.Control.getNoFallbackControl(FORMAT_DEFAULT));
-                loadTranslationsFromBundle(bundle, translations);
-            } catch (MissingResourceException e) {
-                handleMissingResourceException(locale, translations);
-            }
-        }
-    }
-
-    private void processJarResource(URL resource, Locale locale, Properties translations) {
-        try {
-            String decodedPath = URLDecoder.decode(resource.getPath(), StandardCharsets.UTF_8.name());
-            String jarPath = decodedPath.substring(5, decodedPath.indexOf("!"));
-            try (JarFile jarFile = new JarFile(jarPath)) {
-                Enumeration<JarEntry> entries = jarFile.entries();
-                while (entries.hasMoreElements()) {
-                    JarEntry entry = entries.nextElement();
-                    String entryName = entry.getName();
-                    if (entryName.startsWith(LABELS_PATH) && entryName.endsWith(".properties")) {
-                        String[] parts = entryName.split("/");
-                        if (parts.length >= 3) {
-                            String[] lastParts = parts[2].split("_");
-                            if (lastParts.length >= 2) {
-                                String baseName = parts[0] + "." + parts[1] + "." + lastParts[0] + "_" + lastParts[1];
-                                try {
-                                    ResourceBundle bundle = ResourceBundle.getBundle(baseName, locale, ResourceBundle.Control.getNoFallbackControl(FORMAT_DEFAULT));
-                                    loadTranslationsFromBundle(bundle, translations);
-                                } catch (MissingResourceException e) {
-                                    System.err.println("Resource bundle not found: " + baseName);
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void loadTranslationsFromBundle(ResourceBundle bundle, Properties translations) {
-        for (String key : bundle.keySet()) {
-            translations.put(key, bundle.getString(key));
-        }
-    }
-
-    private void handleMissingResourceException(@NotNull Locale locale, Properties translations) {
-        System.err.println("Unsupported language tag: " + locale.toLanguageTag());
-        List<String> supportedLanguageTags = this.getSupportedLanguageTags();
-        if (!supportedLanguageTags.isEmpty()) {
-            String fallbackLanguageTag = supportedLanguageTags.get(0);
-            System.err.println("Falling back to: " + fallbackLanguageTag);
-            Locale fallbackLocale = Locale.forLanguageTag(fallbackLanguageTag);
-            try {
-                Enumeration<URL> resources = getClass().getClassLoader().getResources(LABELS_PATH);
-                while (resources.hasMoreElements()) {
-                    URL resource = resources.nextElement();
-                    if (resource.getProtocol().equals("jar")) {
-                        processJarResource(resource, fallbackLocale, translations);
-                    } else {
-                        processResource(resource, fallbackLocale, translations);
-                    }
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
     }
 }

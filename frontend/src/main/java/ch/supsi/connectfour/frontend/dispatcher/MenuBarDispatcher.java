40bc60533d6c04d0bc270cb5a1979db02c50da6c
galassoo <160861453+galassoo@users.noreply.github.com>
2024-05-30 23:26:30 +0200

    Comments and minor code adjustments in frontend dispatchers

--- frontend/src/main/java/ch/supsi/connectfour/frontend/dispatcher/MenuBarDispatcher.java
+++ frontend/src/main/java/ch/supsi/connectfour/frontend/dispatcher/MenuBarDispatcher.java
@@ -4,13 +4,16 @@ import ch.supsi.connectfour.frontend.controller.*;
 import javafx.fxml.FXML;
 import javafx.scene.control.MenuItem;
 
+//OK
 public class MenuBarDispatcher {
+    /* controllers */
     private static final ConnectFourFrontendController connectFourFrontendController;
     private static final AboutController aboutController;
     private static final PreferencesFrontendController preferencesFrontendController;
     private static final HelpController helpController;
     private static final ApplicationExitController exitController;
 
+    //initialize fields
     static {
         connectFourFrontendController = ConnectFourFrontendController.getInstance();
         aboutController = AboutController.getInstance();
@@ -19,37 +22,59 @@ public class MenuBarDispatcher {
         exitController = ApplicationExitController.getInstance();
     }
 
+    /* components */
     @FXML
     public MenuItem saveMenuItem;
 
+    /**
+     * handle 'new game' request by delegating it to a suitable controller
+     */
     public void newGame() {
         connectFourFrontendController.manageNew();
     }
 
+    /**
+     * handle 'open game' request by delegating it to a suitable controller
+     */
     public void openGame() {
         connectFourFrontendController.manageOpen();
     }
 
+    /**
+     * handle 'save game' request by delegating it to a suitable controller
+     */
     public void saveGame() {
         connectFourFrontendController.manageSave();
     }
-
+    /**
+     * handle 'save as' request by delegating it to a suitable controller
+     */
     public void saveGameAs() {
         connectFourFrontendController.manageSaveAs();
     }
 
+    /**
+     * handle 'quit' request by delegating it to a suitable controller
+     */
     public void quit() {
         exitController.manageExit();
     }
 
+    /**
+     * handle 'preferences' request by delegating it to a suitable controller
+     */
     public void preferences() {
         preferencesFrontendController.managePreferences();
     }
-
+    /**
+     * handle 'about' request by delegating it to a suitable controller
+     */
     public void showAbout() {
         aboutController.showAboutPopUp();
     }
-
+    /**
+     * handle 'help' request by delegating it to a suitable controller
+     */
     public void showHelp() {
         helpController.showHelpPopUp();
     }
